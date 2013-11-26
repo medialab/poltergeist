be76a506e75156610272831c597bb59b25bc01aa
Daniele Guido <gui.daniele@gmail.com>
2013-11-26 19:06:09 +0100

    fisrt steps, but ghost has been modified...

--- js/general.js
+++ js/general.js
@@ -4,25 +4,95 @@
 
 /*globals jQuery, document */
 (function ($) {
-    "use strict";
+  "use strict";
 
+  /*
+    decorate text string with html tags
+  */
+  function decorate_with_aime(text){
+   
+    var _t =  text
+      .replace(/\[[^\]]*\]/g,function(s){
+        return "<span class='modes'>" + s.replace(/[^\w\[·\]]/g,'') + "</span>"
+      }).replace(/[A-ZÀÁÂÈÉÊÌÍÎÏÇÒÓÔŒÙÚÛ][A-ZÀÁÂÈÉÊÌÍÎÏÇÒÓÔŒÙÚÛ]+/g,function(s){
+        return "<span class='smallcaps'>" + s + "</span>"
+      });
+    return _t;
+  }
+
+
+  /*
+    
+
+
+  */
 	$(document).ready(function(){
+    $('header.menu').scrollToFixed({
+      marginTop: 6
+    });
+    $('#secondary-menu').scrollToFixed({
+      marginTop: 37
+    });
 
-		if ( $( '.single-header' ).length ) {
+    var w_height = $(window).height();
 
-			$( 'body' ).on( 'mousewheel', function() {
-		        if ( $( document ).scrollTop() >= 300 )  {
-		        	$( '.single-header' ).removeClass( 'show' );
-		        	$( '.single-header' ).addClass( 'hide' );
-		        } // End If Statement
-		        if ( ( $( document ).scrollTop() < 300 ) && ( $( document ).scrollTop() > 0 ) && ( $( '.single-header' ).hasClass( 'hide' ) ) )  {
-		        	$( '.single-header' ).removeClass( 'hide' );
-		        	$( '.single-header' ).addClass( 'show' );
-		        } // End If Statement
-		    });
+    /*
+      rescale section to fit window height and add omissis.
+      First section will be smalle because of the header.
+    */
+    $('section').each(function(i, e){
+      var el = $(this);
+      if(i ==0){
 
-		} // End If Statement
+      }else{
+        //el.height()
+      }
+    });
 
+		
+    /*
+      Fill with tweets
+    */
+    $.ajax({
+      url: "http://aime.medialab.sciences-po.fr/tweets-aime.json"
+    }).done(function (data) {
+      var results = data || [],
+          last_tweet,
+          tweet,
+          tweet_date;
+
+      console.log(data);
+
+      if(results.length){
+
+        last_tweet = results.shift();
+        console.log(last_tweet);
+
+        tweet_date = new Date(last_tweet.date * 1000);
+
+        tweet = $('<a/>',{'class':'tweet'}).append(
+          '<div class="published_at">' +
+            tweet_date.getDate() +'.'+ (tweet_date.getMonth() + 1) + '.' + tweet_date.getFullYear() +
+          '</div>' +
+          '<h4><div class="author">' +
+            last_tweet.screenname +
+          '</div>' +
+          '<div class="text">' +
+            decorate_with_aime(last_tweet.message) +
+          '</div></h4>'
+        )
+          .attr('href', last_tweet.link)
+          .attr('target', '_blank');
+
+        $("#tweets")
+          .empty()
+          .append(tweet);
+
+      } else {
+        console.log('no data received fron twitter!');
+      }
+       
+    });
 	});
 
 }(jQuery));
