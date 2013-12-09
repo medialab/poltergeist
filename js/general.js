5dd3764886c7d278929a0fa96e849d729ffcc33f
Daniele Guido <gui.daniele@gmail.com>
2013-12-09 14:48:07 +0100

    timeline styled correctly or quasi

--- js/general.js
+++ js/general.js
@@ -312,13 +312,14 @@
         lang: "en",
         width: "480",
         debug: true,
-        height: "400",//pol.cached.timeline.height(),
+        height: "600",//pol.cached.timeline.height(),
         start_at_slide: '23',
         start_zoom_adjust:  '6',
         source: 'https://docs.google.com/spreadsheet/pub?key=0An3vofkD9W6odEhmRF9GQ3ZzWlYxX3J5Z1VPcnh6aGc&output=html',
         //css: pol.cached.timeline.attr('data-css-url'),     //OPTIONAL PATH TO CSS
         js: pol.cached.timeline.attr('data-js-url'),
-        embed_id: 'timeline'
+        embed_id: 'timeline',
+        
     };
     createStoryJS(timeline_config);
   }
